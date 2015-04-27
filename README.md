dea0b3d0a1c7644bcfcfa206d79b485c60efb5db
hadrihl <hadrihilmi@gmail.com>
2015-04-27 21:49:59 +0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,20 @@
+IoT Analog Temperature Sensor (LM35)
+===================================
+A simple analog temperature sensor (LM35) application within Intel (R) XDK IoT Edition.
+
+App Files
+---------
+* index.html
+* main.js
+* package.json
+* README.md
+
+Library
+-------
+* mraa (https://github.com/intel-iot-devkit/mraa)
+
+* express (http://expressjs.com/)
+
+* socket.io (https://socket.io)
+
+
---
+++ main.js
@@ -0,0 +1,38 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(2222, function() {
+	console.log('listening on *:2222')
+});
+
+// mraa
+var mraa = require("mraa");
+console.log("MRAA version: " + mraa.getVersion());
+
+var a = new mraa.Aio(0);
+
+function capturesensor(socket) {
+    
+    setInterval( function() {
+    var b = a.read();
+    
+    b = b * 0.48826125;
+    console.log("Celcius: " + b);
+    socket.emit("message", b);
+    }, 4000);
+}
+
+io.on('connection', function (socket) {
+    console.log('user is connected');
+    
+    capturesensor(socket);
+    
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
+});
